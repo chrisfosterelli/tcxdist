6412450573f8bc276a3b5ccb2879d0d76ce742e6
Chris Foster <chris.james.foster@gmail.com>
2017-10-31 23:30:10 -0700

    Clean up code implementation

--- tcxdist.py
+++ tcxdist.py
@@ -8,45 +8,46 @@ from lxml import etree
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input')
+parser.add_argument('output')
 parser.add_argument('km')
 
+date_str = '%Y-%m-%dT%H:%M:%S.%fZ'
+ns1 = '{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}'
+ns2 = '{http://www.garmin.com/xmlschemas/ActivityExtension/v2}'
 
 def get_time(trackpoint):
     for child in trackpoint:
-        if child.tag == '{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Time':
-            return datetime.datetime.strptime(child.text, "%Y-%m-%dT%H:%M:%S.%fZ")
+        if child.tag == '{:s}Time'.format(ns1):
+            return datetime.datetime.strptime(child.text, date_str)
+
 
 if __name__ == '__main__':
+
     args = parser.parse_args()
-    input_tree = etree.parse(args.input)
-
-    prefix = '//{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}'
-    lap = prefix + 'Lap'
-    trackpoint = prefix + 'Trackpoint'
-
-    lap = input_tree.find(lap)
-    for child in lap:
-        if child.tag == '{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}DistanceMeters':
-            child.text = '{:.4f}'.format(float(args.km))
-
-    trackpoints = input_tree.findall(trackpoint)
-
-    start = get_time(trackpoints[0])
-    end = get_time(trackpoints[-1])
-    length = (end - start).total_seconds()
+    tree = etree.parse(args.input)
     distance = float(args.km) * 1000
+
+    laps = tree.find('//{:s}Lap'.format(ns1))
+    trackpoints = tree.findall('//{:s}Trackpoint'.format(ns1))
+
+    start, end = get_time(trackpoints[0]), get_time(trackpoints[-1])
+    length = (end - start).total_seconds()
+
+    for lap in laps:
+        if lap.tag == '{:s}DistanceMeters'.format(ns1):
+            lap.text = '{:.4f}'.format(float(args.km))
 
     for trackpoint in trackpoints:
         time = get_time(trackpoint)
         for child in trackpoint:
-            if child.tag == '{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}DistanceMeters':
-                child.text = '{:.4f}'.format(( (time - start).total_seconds() ) / length * distance)
-            if child.tag == '{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Extensions':
+            if child.tag == '{:s}DistanceMeters'.format(ns1):
+                cur_dist = ((time - start).total_seconds()) / length * distance
+                child.text = '{:.4f}'.format(cur_dist)
+            if child.tag == '{:s}Extensions'.format(ns1):
+                el_tpx = etree.Element('{:s}TPX'.format(ns2))
+                el_speed = etree.SubElement(el_tpx, '{:s}Speed'.format(ns2))
+                el_speed.text = '{:.4f}'.format(distance / length)
                 child.remove(child[0])
-                el = etree.Element('jjTPX')
-                b = etree.SubElement(el, 'jjSpeed')
-                b.text = '7.40740'
-                child.append(el)
+                child.append(el_tpx)
 
-    filename = '/tmp/test.tcx'
-    input_tree.write(filename)
+    tree.write(args.output, xml_declaration=True, encoding='utf-8')
